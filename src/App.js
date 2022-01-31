fdae548e2bbf963d0af15d2e90f2317053b838b9
Ankit Mahendru <ankit.mahendru@gmail.com>
2022-01-31 16:25:26 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { forwardRef, useEffect, useRef } from 'react';
+import { HashRouter, NavLink, useLocation } from 'react-router-dom';
+import './styles.css';
+
+const Header = ({ refs }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log('location', location.pathname);
+    switch (location.pathname) {
+      case '/about':
+        scrollSmoothHandler(refs.aboutRef);
+        break;
+      case '/contact':
+        scrollSmoothHandler(refs.contactRef);
+        break;
+      case '/hero':
+        scrollSmoothHandler(refs.heroRef);
+        break;
+
+      default:
+        scrollSmoothHandler(refs.homeRef);
+        break;
+    }
+  }, [location, refs]);
+
+  const scrollSmoothHandler = (ref) => {
+    console.log('Triggered.');
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <NavLink to='/intro' activeClassName='selected'>
+        Intro
+      </NavLink>
+      <NavLink to='/about' activeClassName='selected'>
+        About
+      </NavLink>
+      <NavLink to='/contact' activeClassName='selected'>
+        Contact
+      </NavLink>
+    </>
+  );
+};
+
+const Intro = forwardRef((props, ref) => {
+  return (
+    <section ref={ref}>
+      <h1>Intro Section</h1>
+    </section>
+  );
+});
+
+const About = forwardRef((props, ref) => {
+  return (
+    <section ref={ref}>
+      <h1>About Section</h1>
+    </section>
+  );
+});
+
+const Contact = forwardRef((props, ref) => {
+  return (
+    <section ref={ref}>
+      <h1>Contact Section</h1>
+    </section>
+  );
+});
 
 function App() {
+  const homeRef = useRef(null);
+  const introRef = useRef(null);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={homeRef} className='App'>
+      <HashRouter>
+        <Header refs={{ aboutRef, contactRef, introRef, homeRef }} />
+        <Intro ref={introRef} />
+        <About ref={aboutRef} />
+        <Contact ref={contactRef} />
+      </HashRouter>
     </div>
   );
 }
